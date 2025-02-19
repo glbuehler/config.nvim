2bbe9fbe3847af3742e24239a34ea00a0de8bae9
Gideon Bühler <gideonbuehler18@gmail.com>
2025-02-19 16:50:24 +0100

    surround with parentesese

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -8,3 +8,5 @@ vim.keymap.set('n', '<C-k>', 'kddpk')
 vim.keymap.set('n', 'gj', vim.diagnostic.goto_next, { desc = 'Next diagnostic' })
 vim.keymap.set('n', 'gk', vim.diagnostic.goto_prev, { desc = 'Next diagnostic' })
 vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, { desc = 'Lsp: [A]ctions' })
+
+vim.keymap.set('v', 'g(', 'di(<C-c>pli)<C-c>', { desc = 'Surround with parenthesese' })
