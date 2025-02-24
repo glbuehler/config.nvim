bec38ae7df2ea7cf2e7eb0ea7a205b6f31d044d2
Gideon Bühler <gideonbuehler18@gmail.com>
2025-02-24 22:50:35 +0100

    removed parenthesis surrounding bc doesnt work quite well and added clear line

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -8,5 +8,4 @@ vim.keymap.set('n', '<C-k>', 'kddpk')
 vim.keymap.set('n', 'gj', vim.diagnostic.goto_next, { desc = 'Next diagnostic' })
 vim.keymap.set('n', 'gk', vim.diagnostic.goto_prev, { desc = 'Next diagnostic' })
 vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, { desc = 'Lsp: [A]ctions' })
-
-vim.keymap.set('v', 'g(', 'di(<C-c>pli)<C-c>', { desc = 'Surround with parenthesese' })
+vim.keymap.set('n', 'dD', 'cc<C-c>', { desc = 'Clear line' })
